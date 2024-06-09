c57c7cf3489123ca6ac7379f7815d3cf8974eecb
inteltone <inteltone@gmail.com>
2024-06-09 11:34:41 +0300

    commit

--- pages/search-vin-step2.vue
+++ pages/search-vin-step2.vue
@@ -3,6 +3,124 @@ const rowItems = ref(['AUDI', 'A6', '2.4 AVANT', '2001', 'Европа', 'ALF V6
 // классификатор
 const tabs = ref(['Общий', 'От производителя'])
 const tabIndex = ref(0)
+function tabClicked(index) {
+	tabIndex.value = tabIndex.value > 0 ? 0 : 1
+	category1Index.value = undefined
+	category2Index.value = undefined
+	category3Index.value = undefined
+	optionIndex.value = undefined
+	showContent.value = 0
+}
+// для классификатора
+const classifier = ref([
+	{
+		name: 'Детали для ТО',
+		categories: [
+			{name: 'Фильтр масляный 1',options: ['Фильтр 11','Фильтр 12','Фильтр 13']},
+			{name: 'Фильтр масляный 2',options: ['Фильтр 21','Фильтр 22']},
+			{
+				name: 'Фильтр масляный 3',
+				categories: [
+					{name: 'Фильтр масляный 4',options: ['Фильтр 41','Фильтр 42','Фильтр 43']},
+					{name: 'Фильтр масляный 5',options: ['Фильтр 51','Фильтр 52']},
+				],
+				name: 'Фильтр масляный 6',
+				categories: [
+					{name: 'Фильтр масляный 7',options: ['Фильтр 71','Фильтр 72','Фильтр 73']},
+					{name: 'Фильтр масляный 8',options: ['Фильтр 81','Фильтр 82']},
+				],
+			},
+		]
+	},
+	{
+		name: 'Двигатель',		
+		categories: [
+			{name: 'Поршни 1',options: ['Поршень 11','Поршень 12','Поршень 13']},
+			{name: 'Поршни 2',options: ['Поршень 21','Поршень 22']},
+			{
+				name: 'Поршни 3',
+				categories: [
+					{name: 'Поршни 4',options: ['Поршень 41','Поршень 42','Поршень 43']},
+					{name: 'Поршни 5',options: ['Поршень 51','Поршень 52']},
+				],
+				name: 'Поршни 6',
+				categories: [
+					{name: 'Поршни 7',options: ['Поршень 71','Поршень 72','Поршень 73']},
+					{name: 'Поршни 8',options: ['Поршень 81','Поршень 82']},
+				],
+			},
+		]
+	},
+])
+const classifier2 = ref([
+	{
+		name: 'Топливная система',
+		categories: [
+			{name: 'Топливный бак 1',options: ['Бак 11','Бак 12','Бак 13']},
+			{name: 'Топливный бак 2',options: ['Бак 21','Бак 22']},
+			{
+				name: 'Топливный бак 3',
+				categories: [
+					{name: 'Топливный бак 4',options: ['Бак 41','Бак 42','Бак 43']},
+					{name: 'Топливный бак 5',options: ['Бак 51','Бак 52']},
+				],
+				name: 'Топливный бак 6',
+				categories: [
+					{name: 'Топливный бак 7',options: ['Бак 71','Бак 72','Бак 73']},
+					{name: 'Топливный бак 8',options: ['Бак 81','Бак 82']},
+				],
+			},
+		]
+	},	
+])
+// 1 УРОВЕНЬ - ОТКР./ЗАКР.
+const category1Index = ref(undefined)
+function category1Clicked(index) {
+	if (category1Index.value === index) {
+		category1Index.value = undefined
+		category2Index.value = undefined
+		category3Index.value = undefined
+		optionIndex.value = undefined
+		return
+	}
+	category2Index.value = undefined
+	category3Index.value = undefined
+	optionIndex.value = undefined
+	category1Index.value = index
+	showContent.value = 0
+}
+// 2 УРОВЕНЬ - ОТКР./ЗАКР.
+const category2Index = ref(undefined)
+function category2Clicked(index) {
+	if (category2Index.value === index) {
+		category2Index.value = undefined
+		category3Index.value = undefined
+		optionIndex.value = undefined
+		return
+	}
+	category3Index.value = undefined
+	optionIndex.value = undefined
+	category2Index.value = index
+	showContent.value = 1
+}
+// 3 УРОВЕНЬ - ОТКР./ЗАКР.
+const category3Index = ref(undefined)
+function category3Clicked(index) {
+	if (category3Index.value === index) {
+		category3Index.value = undefined
+		optionIndex.value = undefined
+		return
+	}
+	optionIndex.value = undefined
+	category3Index.value = index
+	showContent.value = 1
+}
+// ПОКАЗ КОНЕЧНОГО ПРОДУКТА
+const optionIndex = ref(undefined)
+function optionClicked(index) {	
+	optionIndex.value = index
+	showContent.value = 2
+}
 // для карточек категорий
 const categories = ref([
 	{name: 'Электрика', img: '01'},
@@ -22,29 +140,6 @@ const categories = ref([
 ])
 // показ контента при кликах на классификаторе
 const showContent = ref(0)
-onMounted(()=>{
-	let options = document.querySelectorAll('.classifier__option')
-	let spans = document.querySelectorAll('.classifier__options span')
-	options.forEach(option => {
-		option.addEventListener('click', (e) => {			
-			spans.forEach(span => span.classList.remove('active'))
-			let parent = e.currentTarget.parentElement
-			if (parent.parentElement.classList.contains('classifier__panel')) {
-				showContent.value = 0
-			} else showContent.value = 1
-			parent.classList.toggle('active')
-			parent.firstElementChild.classList.toggle('active')
-			parent.querySelector('i').classList.toggle('rotate')
-		})
-	})
-	spans.forEach(span => {
-		span.addEventListener('click', (e) => {
-			showContent.value = 2
-			spans.forEach(span => span.classList.remove('active'))			
-			e.currentTarget.classList.add('active')
-		})
-	})
-})
 </script>
 <template>
 	<NuxtLayout>
@@ -69,93 +164,183 @@ onMounted(()=>{
 						<button><i class="icon-search"></i></button>
 					</form>
 				</div>
+				<!-- НАЧАЛО КЛАССИФИКАТОРА -->
 				<div class="classifier">
 					<p class="classifier__title">Классификатор</p>
-					<div class="classifier__tabs-container">
+					<div class="classifier__container">
+						<!-- ТАБЫ КЛАССИФИКАТОРА -->
 						<div class="classifier__tabs">
 							<span 
 								v-for="tab,index in tabs" 
 								:key="tab"
-								@click="tabIndex = tabIndex > 0 ? 0 : 1"
+								@click="tabClicked(index)"
 								class="classifier__tab"
 								:class="{active: tabIndex === index}"
 							>{{tab}}</span>							
 						</div>
 						<div class="classifier__panels">
+							<!-- 1 КЛАССИФИКАТОР - ОБЩИЙ -->
 							<div 
-							class="classifier__panel" 
-							v-for="panel,index in tabs.length"
-							:class="{active: tabIndex === index}"
+								class="classifier__panel" 							
+								:class="{active: tabIndex === 0}"
 							>
-								<div class="classifier__select">
-									<span class="classifier__option">Детали для ТО<i class="icon-arrow-down"></i></span>
-									<div class="classifier__select">
-										<span class="classifier__option">Фильтр масляный<i class="icon-arrow-down"></i></span>
-										<div class="classifier__options">
-											<span>Фильтр масляный</span>											
-											<span>Фильтр масляный</span>
+								<!-- 1 УРОВЕНЬ ----------------------------------------------------------------->
+								<div class="classifier__select" 
+									v-for="level1,index in classifier" :key="index"
+									:class="{active: category1Index === index}"
+								>
+									<span class="classifier__category" 
+										@click="category1Clicked(index)"
+										:class="{active: category1Index === index}"
+									>
+										{{level1.name}}
+										<i class="icon-arrow-down" :class="{rotate: category1Index === index}"></i>
+									</span>	
+									<template v-if="level1.options">
+										<div class="classifier__options" :class="{rotate: category1Index === index}">
+											<span v-for="option,index in level1.options"
+												@click="optionClicked(index)"
+												:class="{active: optionIndex === index}"
+											>
+												{{option}}
+											</span>												
 										</div>
-									</div>
-									<div class="classifier__select">
-										<span class="classifier__option">Фильтр масляный<i class="icon-arrow-down"></i></span>
-										<div class="classifier__options">
-											<span>Фильтр масляный</span>											
-											<span>Фильтр масляный</span>
+									</template>	
+									<template v-if="level1.categories">
+										<!-- 2 УРОВЕНЬ ------------------------------------------------------------>
+										<div class="classifier__select" 
+											v-for="level2,index in level1.categories" :key="index"	
+											:class="{active: category2Index === index}"										
+										>
+											<span class="classifier__category" 
+												@click="category2Clicked(index)"
+												:class="{active: category2Index === index}"
+											>
+												{{level2.name}}
+												<i class="icon-arrow-down" :class="{rotate: category2Index === index}"></i>
+											</span>
+											<template v-if="level2.options">
+												<div class="classifier__options" :class="{rotate: category2Index === index}">
+													<span v-for="option,index in level2.options"
+														@click="optionClicked(index)"
+														:class="{active: optionIndex === index}"
+													>
+														{{option}}
+													</span>												
+												</div>
+											</template>
+											<template v-if="level2.categories">
+												<!-- 3 УРОВЕНЬ ------------------------------------------------------>
+												<div class="classifier__select" 
+													v-for="level3,index in level2.categories" :key="index"
+													:class="{active: category3Index === index}"
+												>
+													<span class="classifier__category" 
+														@click="category3Clicked(index)"
+														:class="{active: category3Index === index}"
+													>
+														{{level3.name}}
+														<i class="icon-arrow-down" :class="{rotate: category3Index === index}"></i>
+													</span>
+													<template v-if="level3.options">
+														<div class="classifier__options" :class="{rotate: category3Index === index}">
+															<span v-for="option,index in level3.options"
+																@click="optionClicked(index)"
+																:class="{active: optionIndex === index}"
+															>
+																{{option}}
+															</span>												
+														</div>
+													</template>												
+												</div>
+											</template>													
 										</div>
-									</div>
-									<div class="classifier__select">
-										<span class="classifier__option">Фильтр масляный<i class="icon-arrow-down"></i></span>
-										<div class="classifier__select">
-											<span class="classifier__option">Фильтр масляный<i class="icon-arrow-down"></i></span>
-											<div class="classifier__options">
-												<span>Фильтр масляный</span>
-												<span>Фильтр масляный</span>												
-											</div>
+									</template>							
+								</div>								
+							</div>
+							<!-- 2 КЛАССИФИКАТОР - ОТ ПРОИЗВОДИТЕЛЯ -->
+							<div 
+								class="classifier__panel" 							
+								:class="{active: tabIndex === 1}"
+							>
+								<!-- 1 УРОВЕНЬ ----------------------------------------------------------------->
+								<div class="classifier__select" 
+									v-for="level1,index in classifier2" :key="index"
+									:class="{active: category1Index === index}"
+								>
+									<span class="classifier__category" 
+										@click="category1Clicked(index)"
+										:class="{active: category1Index === index}"
+									>
+										{{level1.name}}
+										<i class="icon-arrow-down" :class="{rotate: category1Index === index}"></i>
+									</span>	
+									<template v-if="level1.options">
+										<div class="classifier__options" :class="{rotate: category1Index === index}">
+											<span v-for="option,index in level1.options"
+												@click="optionClicked(index)"
+												:class="{active: optionIndex === index}"
+											>
+												{{option}}
+											</span>												
 										</div>
-										<div class="classifier__select">
-											<span class="classifier__option">Фильтр масляный<i class="icon-arrow-down"></i></span>
-											<div class="classifier__options">
-												<span>Фильтр масляный</span>
-												<span>Фильтр масляный</span>												
-											</div>
+									</template>	
+									<template v-if="level1.categories">
+										<!-- 2 УРОВЕНЬ ------------------------------------------------------------>
+										<div class="classifier__select" 
+											v-for="level2,index in level1.categories" :key="index"	
+											:class="{active: category2Index === index}"										
+										>
+											<span class="classifier__category" 
+												@click="category2Clicked(index)"
+												:class="{active: category2Index === index}"
+											>
+												{{level2.name}}
+												<i class="icon-arrow-down" :class="{rotate: category2Index === index}"></i>
+											</span>
+											<template v-if="level2.options">
+												<div class="classifier__options" :class="{rotate: category2Index === index}">
+													<span v-for="option,index in level2.options"
+														@click="optionClicked(index)"
+														:class="{active: optionIndex === index}"
+													>
+														{{option}}
+													</span>												
+												</div>
+											</template>
+											<template v-if="level2.categories">
+												<!-- 3 УРОВЕНЬ ------------------------------------------------------>
+												<div class="classifier__select" 
+													v-for="level3,index in level2.categories" :key="index"
+													:class="{active: category3Index === index}"
+												>
+													<span class="classifier__category" 
+														@click="category3Clicked(index)"
+														:class="{active: category3Index === index}"
+													>
+														{{level3.name}}
+														<i class="icon-arrow-down" :class="{rotate: category3Index === index}"></i>
+													</span>
+													<template v-if="level3.options">
+														<div class="classifier__options" :class="{rotate: category3Index === index}">
+															<span v-for="option,index in level3.options"
+																@click="optionClicked(index)"
+																:class="{active: optionIndex === index}"
+															>
+																{{option}}
+															</span>												
+														</div>
+													</template>												
+												</div>
+											</template>													
 										</div>
-										<div class="classifier__select">
-											<span class="classifier__option">Фильтр масляный<i class="icon-arrow-down"></i></span>
-											<div class="classifier__options">
-												<span>Фильтр масляный</span>
-												<span>Фильтр масляный</span>												
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="classifier__select">
-									<span class="classifier__option">Двигатель<i class="icon-arrow-down"></i> </span>
-									<div class="classifier__select">
-										<span class="classifier__option">Поршни<i class="icon-arrow-down"></i> </span>
-										<div class="classifier__options">
-											<span>Поршни</span>
-											<span>Поршни</span>
-											<span>Поршни</span>
-											<span>Поршни</span>
-										</div>
-									</div>
-									<div class="classifier__select">
-										<span class="classifier__option">Поршни<i class="icon-arrow-down"></i> </span>
-										<div class="classifier__select">
-											<span class="classifier__option">Поршни<i class="icon-arrow-down"></i> </span>
-											<div class="classifier__options">
-												<span>Поршни</span>
-												<span>Поршни</span>
-												<span>Поршни</span>
-												<span>Поршни</span>
-											</div>
-										</div>
-									</div>
-								</div>
+									</template>							
+								</div>								
 							</div>				
 						</div>
 					</div>
-				</div>				
+				</div>
+				<!-- КОНЕЦ КЛАССИФИКАТОРА -->				
 			</div>
 			<div class="search-vin-res__content">
 				<div class="auto-grid mbe-47" v-show="showContent === 0">
@@ -275,8 +460,7 @@ onMounted(()=>{
 			color: var(--clr-orange-100);
 			font-weight: var(--fw-b);
 		}
-	}
-	&__panels{}
+	}	
 	&__panel{
 		display: none;
 		border-block: 1px solid var(--clr-gray);
@@ -289,13 +473,13 @@ onMounted(()=>{
 	}
 	&__select{
 		&>.classifier__select{
-			margin-inline-start: var(--pad-i-s);
 			display: none;
+			margin-inline-start: var(--pad-i-s);
 		}		
 		&.active>.classifier__select{
 			display: block;			
 		}
-		&.active>.classifier__option{
+		&.active>.classifier__category{
 			border-block-end: 1px solid var(--clr-gray);		
 		}				
 		&.active>.classifier__options{
@@ -323,10 +507,31 @@ onMounted(()=>{
 			font-weight: var(--fw-b);
 		}
 	}	
+	&__category{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		block-size: 38px;
+		padding-inline-end: 24px;		
+		cursor: pointer;		
+		transition: color var(--tr);
+		i{
+			transition: transform var(--tr);
+		}
+		i.rotate{
+			transform: rotate(-180deg);
+		}
+		&:hover{
+			color: var(--clr-orange-100);
+		}
+		&.active{
+			font-weight: var(--fw-b);
+		}
+	}	
 	&__options{
-		display: grid;
-		padding-inline-start: var(--pad-i-s);
 		display: none;
+		padding-inline-start: var(--pad-i-s);
+		
 		&>*{
 			padding-block: 8px;			
 			cursor: pointer;
@@ -337,10 +542,13 @@ onMounted(()=>{
 			&:not(:last-child){
 				border-block-end: 1px solid var(--clr-gray);	
 			}
-			&.active{
+			&.active{				
 				color: var(--clr-orange-100);
 				font-weight: var(--fw-b);
 			}
+		}
+		&.active{
+			display: grid;
 		}
 		
 	}
