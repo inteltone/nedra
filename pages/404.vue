989e47b548cae0ce28ea7173d8a880693949a68b
inteltone <inteltone@gmail.com>
2024-06-15 15:03:40 +0300

    commit

---
+++ pages/404.vue
@@ -0,0 +1,13 @@
+<template>
+	<NuxtLayout>		
+		<div class="_404">
+			<div class="_404__title-block">
+				<h1 class="_404__title">404</h1>
+				<p class="_404__text">Такой страницы не существует</p>
+			</div>
+			<div class="_404__img">
+				<img src="/images/misc/404.jpg" alt="">
+			</div>
+		</div>		
+	</NuxtLayout>
+</template>
